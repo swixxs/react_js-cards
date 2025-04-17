903df30e4187803fd34aaac45636fae8af56ae17
Pshennikov_Vitaliy <pshennikovvit@gmail.com>
2025-04-18 01:32:21 +0700

    input, eslint, memo

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,6 +8,8 @@ import { useFetch } from "../../hooks/useFetch";
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
 
+  const [searchValue, setSearchValue] = useState("");
+
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
     const questions = await response.json();
@@ -20,8 +22,14 @@ export const HomePage = () => {
     getQuestions("react");
   }, []);
 
+  const onSearchValueHandler = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <>
+      <input type="text" value={searchValue} onChange={onSearchValueHandler} />
+
       {isLoading && <Loader />}
       {error && <p>${error}</p>}
       <QuestionCardList cards={questions} />
