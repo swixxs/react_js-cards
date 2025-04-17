03f8b19bc159cd890141d0f6c4cc475cdc09e280
Pshennikov_Vitaliy <pshennikovvit@gmail.com>
2025-04-18 06:08:31 +0700

    upd style, sort logic

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -5,10 +5,10 @@ export const SearchIcon = (props) => {
         d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
         stroke="currentColor"
         fill="none"
-        stroke-width="2"
-        fill-rule="evenodd"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        fillRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></path>
     </svg>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { QuestionCardList } from "../../components/QuestionCardList";
 import { Loader } from "../../components/Loader";
 import { API_URL } from "../../constants";
@@ -9,6 +9,7 @@ import { SearchInput } from "../../components/SearchInput";
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [sortSelectValue, setSortSelectValue] = useState("");
 
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
@@ -18,23 +19,41 @@ export const HomePage = () => {
     return questions;
   });
 
+  const cards = useMemo(() => {
+    return questions.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLocaleLowerCase()));
+  }, [questions, searchValue]);
+
   useEffect(() => {
-    getQuestions("react");
-  }, []);
+    getQuestions(`react?${sortSelectValue}`);
+  }, [sortSelectValue]);
 
   const onSearchValueHandler = (e) => {
     setSearchValue(e.target.value);
+  };
+  const onSortSelectChagneHandler = (e) => {
+    setSortSelectValue(e.target.value);
   };
 
   return (
     <>
       <div className={cls.controlsContainer}>
         <SearchInput value={searchValue} onChange={onSearchValueHandler} />
+
+        <select value={sortSelectValue} onChange={onSortSelectChagneHandler} className={cls.select}>
+          <option value="">Sort by:</option>
+          <hr />
+          <option value="_sort=level">Level ASC</option>
+          <option value="_sort=-level">Level DESC</option>
+          <option value="_sort=completed">Completed ASC</option>
+          <option value="_sort=-completed">Completed DESC</option>
+        </select>
       </div>
 
       {isLoading && <Loader />}
-      {error && <p>${error}</p>}
-      <QuestionCardList cards={questions} />
+      {error && <p>{error}</p>}
+      {cards.length === 0 && <p className={cls.noCardsInfo}>No cards...</p>}
+
+      <QuestionCardList cards={cards} />
     </>
   );
 };
