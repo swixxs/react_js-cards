76927d4ad73881a7b1281c762e8668cdb3605e63
Pshennikov_Vitaliy <pshennikovvit@gmail.com>
2025-04-19 21:15:27 +0700

    react toast

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,23 +1,28 @@
 import { Outlet } from "react-router-dom";
 import cls from "./MainLayout.module.css";
 import { Header } from "../Header";
+import { ToastContainer } from "react-toastify";
 
 export const MainLayout = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className={cls.mainLayout}>
-      <Header />
+    <>
+      <div className={cls.mainLayout}>
+        <Header />
 
-      <div className={cls.mainWrapper}>
-        <main className={cls.main}>
-          <Outlet />
-        </main>
-        <footer className={cls.footer}>
-          React Question Cards Application | {currentYear} <br />
-          by Vitaliy Ps
-        </footer>
+        <div className={cls.mainWrapper}>
+          <main className={cls.main}>
+            <Outlet />
+          </main>
+          <footer className={cls.footer}>
+            React Question Cards Application | {currentYear} <br />
+            by Vitaliy Ps
+          </footer>
+        </div>
       </div>
-    </div>
+
+      <ToastContainer />
+    </>
   );
 };
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { delayFn } from "../helpers/delayFn";
+import { toast } from "react-toastify";
 
 export const useFetch = (callback) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -9,12 +10,13 @@ export const useFetch = (callback) => {
     try {
       setIsLoading(true);
       setError("");
-      await delayFn(1000);
+      await delayFn();
 
       const response = await callback(arg);
       return response;
     } catch (error) {
       setError(error.message);
+      toast.error(error.message);
     } finally {
       setIsLoading(false);
     }
